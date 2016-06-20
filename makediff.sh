93b2eab8e3c4cd250cbd91115d48250f1f7a1be1
aih <aih@tabulaw.com>
2016-06-19 18:26:21 -0700

    Pass argument to applescript for document name

--- makediff.sh
+++ makediff.sh
@@ -6,5 +6,5 @@ git show HEAD:SampleDoc.docx > SampleDoc-prev.docx
 git wdiff HEAD SampleDoc.docx > changes.diff
 ansi-to-html -ntrue changes.diff>changes.html
 git add changes.diff changes.html
-osascript ./CompareDocs.scpt
+osascript ./CompareDocs.scpt "SampleDoc"
 git add ./*
